34f549a82597794d90b55bf7069ee41aa9ffbb5b
jacob m <jacobamaynard@gmail.com>
2023-02-11 15:56:45 -0600

    added js to produce password

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordString="";
+  // var chars = upper || lower || num || special;
+var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var num = ['0','1','2','3','4','5','6','7','8','9']
+var special = ["!#$%&'()*+,-./:;<=>?@[ \ ]^_`{|}~"]
+var chosenChars= [];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword() ;
@@ -8,69 +15,58 @@ function writePassword() {
  
   passwordText.value = password;
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-
-
-
-
-
-
-  var userChoice = window.prompt("Include numbers");
+  var charLength = parseInt(prompt("How many characters you want? Password length must be between 8 to 128"));
+ 
+  if (!(charLength > 8 || charLength < 128 || charLength === "")) {
+    alert("Please choose a length from(0-128)");
+    generatePassword();
+  }else{
+    alert('Length added')
+  var numberChoice = window.confirm("Include numbers");
   
-  var userChoice2 = window.prompt("Include UpperCase Letters");
+  var upperChoice = window.confirm("Include UpperCase Letters");
 
-  var userChoice3 = window.prompt("Include LowerCase Letters");
+  var lowerChoice = window.confirm("Include LowerCase Letters");
 
-  var userChoice4 = window.prompt("Include special characters");
+  var specialChoice = window.confirm("Include special characters");
+    
+    if (numberChoice) {
+      chosenChars.push(...num);
+      console.log(chosenChars)
+    }
+    if (upperChoice) {
+      chosenChars.push(...upper)
+      console.log(chosenChars)
+    }
+    if (lowerChoice) {
+      chosenChars.push(...lower)
+      console.log(chosenChars)
+    }
+    if (specialChoice) {
+      chosenChars.push(...special)
+      console.log(chosenChars)
+    }
 
-
-
-
-
-  var passwordLength = function () {
-    var charLength = parseInt(prompt("How many characters you want? Password length must be between 8 to 128"));
-
-    if (!(charLength > 8 || charLength < 128 || charLength === "")) {
-      alert("Please choose a length from(0-128)");
-      return passwordLength();
+    for (var i = 0; i < charLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chosenChars.length);
+    console.log(randomNumber)
+    passwordString += chosenChars[randomNumber]
     }
   }
 
-  console.log("pressed");
-
-  var password="";
-
-  // var chars = upper || lower || num || special;
-  var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  var num = ['0','1','2','3','4','5','6','7','8','9']
-  var special = ["!#$%&'()*+,-./:;<=>?@[ \ ]^_`{|}~"]
-
-  console.log = "whyy";
 
 
 
-
-  // for (var i = 0; i < charLength; i++) {
-  //   var randomNumber = math.floor(math.random() * chars.length);
-  //   password += chars.substring(randomNumber, randomNumber + 1);
-  // }
-
-
-
-
-
-  document.getElementById("password").value = password
   return passwordString;
 }
 
 
 
 
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 // must return a string value that is the password
